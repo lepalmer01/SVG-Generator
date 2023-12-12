9d025a79086b83c6f028a7457b0c6f709b2e0717
LE Palmer <lpalmer@ucsd.edu>
2023-12-12 14:57:24 -0800

    created function to prompt user for input, create a logo baed on input and save it to a file

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+function generateLogo() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the text:',
+            validate: input => input.length <= 3 || "Text cannot be more than 3 characters"
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color (keyword or hexadecimal):',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['circle', 'triangle', 'square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color (keyword or hexadecimal):',
+        },
+    ])
+    .then(({ text, textColor, shape, shapeColor }) => {
+        let shapeType;
+
+        switch (shape) {
+            case 'circle':
+                shapeType = new Circle;
+                break;
+            case 'triangle':
+                shapeType = new Triangle;
+                break;
+            default:
+                shapeType = new Square
+                break;
+            }
+
+            shapeType.setColor(shapeColor)
+
+            const logo = new SVGGenerator();
+            logo.setText(text, textColor)
+            logo.setShape(shapeType)
+
+            fs.writeFileSync('./examples/logo.svg', logo.render());
+            console.log('Generated logo.svg')
+       })
+    }
+    
+    generateLogo();
+
+
